21ef2bebd6bcb16875acc445cae7be3c1d644b8a
J.P. Cranford <philipcranford4@gmail.com>
2020-11-05 22:09:43 -0700

    Update ChurchOfJesusChrist - Safari Pinned Tab.user.js

--- ChurchOfJesusChrist - Safari Pinned Tab.user.js
+++ ChurchOfJesusChrist - Safari Pinned Tab.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Safari Pinned Tab: ChurchOfJesusChrist.org
 // @author       J.P. Cranford
-// @version      0.12
+// @version      0.16
 // @description  Adds support for Safari's Pinned Tab and Touch Bar button to the Church's website.
-// @match        *://*.churchofjesuschrist.org*
+// @match        *://*.churchofjesuschrist.org/*
 // @supportURL   https://github.com/jpcranford/userscripts/issues
 // @run-at document-start
 // ==/UserScript==
@@ -14,12 +14,12 @@ window.addEventListener("load", function() {
 
     icon.setAttribute('rel', 'mask-icon');
     icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/Q2h1cmNoT2ZKZXN1c0NocmlzdC5vcmc.svg');
-    icon.setAttribute('color', '#01B6D1');
+    icon.setAttribute('color', '#006184');
 // blue 40: #003057
 // blue 35: #005581
 // Blue 30: #006184
-// blue 25: #007d9f (passes only WCAG AA on Large Size)
-// blue 20: #01b6d1 (passes all on black) <<< current test
+// blue 25: #007d9f (passes only WCAG AA on Large Size) <<< current test
+// blue 20: #01b6d1 (passes all on black)
 // plex: #cc7b19 (passes all but normal size AAA on black)
 // twitch: #9146FF (passes all but normal size AAA on black)
     head.appendChild(icon);
