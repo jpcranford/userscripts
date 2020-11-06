950191552957a8aa170b8ad48ddbb75e0bcc7f70
J.P. Cranford <philipcranford4@gmail.com>
2020-11-05 18:07:24 -0700

    Add Pocket script

---
+++ Pocket - Safari Pinned Tab.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Safari Pinned Tab: Pocket
+// @version      0.5
+// @description  Adds support for Safari's Pinned Tab and Touch Bar button to Pocket.
+// @author       J.P. Cranford
+// @include      http*://app.getpocket.*
+// ==/UserScript==
+
+window.addEventListener("load", function() {
+    var head = document.getElementsByTagName('head')[0];
+    var icon = document.createElement('link');
+
+    icon.setAttribute('rel', 'mask-icon');
+    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/UG9ja2V0.svg');
+    icon.setAttribute('color', '#EF4056');
+
+    head.appendChild(icon);
+});
