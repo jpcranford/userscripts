e527af896e7aaed30de565ec9675fd17508cdd57
J.P. Cranford <philipcranford4@gmail.com>
2020-11-06 16:05:33 -0700

    Change to darker color, test black-mode SVG

--- ChurchOfJesusChrist - Safari Pinned Tab.user.js
+++ ChurchOfJesusChrist - Safari Pinned Tab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Safari Pinned Tab: ChurchOfJesusChrist.org
 // @author       J.P. Cranford
-// @version      0.16
+// @version      0.18
 // @description  Adds support for Safari's Pinned Tab and Touch Bar button to the Church's website.
 // @match        *://*.churchofjesuschrist.org/*
 // @supportURL   https://github.com/jpcranford/userscripts/issues
@@ -13,14 +13,8 @@ window.addEventListener("load", function() {
     var icon = document.createElement('link');
 
     icon.setAttribute('rel', 'mask-icon');
-    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/Q2h1cmNoT2ZKZXN1c0NocmlzdC5vcmc.svg');
-    icon.setAttribute('color', '#006184');
-// blue 40: #003057
-// blue 35: #005581
-// Blue 30: #006184
-// blue 25: #007d9f (passes only WCAG AA on Large Size) <<< current test
-// blue 20: #01b6d1 (passes all on black)
-// plex: #cc7b19 (passes all but normal size AAA on black)
-// twitch: #9146FF (passes all but normal size AAA on black)
+    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/simplified_black.svg');
+    icon.setAttribute('color', '#007d9f');
+
     head.appendChild(icon);
 });
