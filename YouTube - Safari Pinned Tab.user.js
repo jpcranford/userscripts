4e7accfca10224a616d44a386b2eaf26124a9119
J.P. Cranford <philipcranford4@gmail.com>
2020-11-05 17:40:45 -0700

    Add YouTube script

---
+++ YouTube - Safari Pinned Tab.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Safari Pinned Tab: YouTube
+// @version      1.0
+// @description  Adds support for Safari's Pinned Tab and Touch Bar button to YouTube.
+// @author       J.P. Cranford
+// @include      http*://www.youtube.*
+// @grant        none
+// ==/UserScript==
+
+window.addEventListener("load", function() {
+    var head = document.getElementsByTagName('head')[0];
+    var icon = document.createElement('link');
+
+    icon.setAttribute('rel', 'mask-icon');
+    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/WW91VHViZQ.svg');
+    icon.setAttribute('color', '#FF0000');
+
+    head.appendChild(icon);
+});
