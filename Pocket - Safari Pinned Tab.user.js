dd4af6e050cbde7c7dcfdbf8479e9b09f7e860e9
J.P. Cranford <philipcranford4@gmail.com>
2020-11-05 21:36:28 -0700

    Update Pocket - Safari Pinned Tab.user.js

--- Pocket - Safari Pinned Tab.user.js
+++ Pocket - Safari Pinned Tab.user.js
@@ -1,17 +1,16 @@
 // ==UserScript==
 // @name         Safari Pinned Tab: Pocket
 // @author       J.P. Cranford
-// @version      1.5
+// @version      1.11
 // @description  Adds support for Safari's Pinned Tab and Touch Bar button to Pocket.
-// @icon         https://app.getpocket.com/favicon.ico
 // @match        *://*.getpocket.com*
 // @supportURL   https://github.com/jpcranford/userscripts/issues
 // @updateURL    https://raw.githubusercontent.com/jpcranford/userscripts/main/Pocket%20-%20Safari%20Pinned%20Tab.user.js
-// @run-at       document-start
+// @run-at document-start
 // ==/UserScript==
 
 window.addEventListener("load", function() {
-    var head = document.head.id;
+    var head = document.getElementsByTagName('head')[0];
     var icon = document.createElement('link');
 
     icon.setAttribute('rel', 'mask-icon');
