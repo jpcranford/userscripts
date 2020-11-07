afc363e587d2f1d31e02867681f5ddaf411c3888
J.P. Cranford <philipcranford4@gmail.com>
2020-11-06 17:16:50 -0700

    Create ChurchOfJesusChrist - Apple Touch Icon.user.js

---
+++ ChurchOfJesusChrist - Apple Touch Icon.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Apple Touch Icon: ChurchOfJesusChrist.org
+// @author       J.P. Cranford
+// @version      1.0
+// @description  Adds an Apple Touch Icon for all pages on the Church's main site, using images from their Facebook page.
+// @match        *://*.churchofjesuschrist.org/*
+// @supportURL   https://github.com/jpcranford/userscripts/issues
+// @run-at document-start
+// ==/UserScript==
+
+window.addEventListener("load", function() {
+    var head = document.getElementsByTagName('head')[0];
+    var icon = document.createElement('link');
+
+    icon.setAttribute('rel', 'apple-touch-icon');
+    icon.setAttribute('sizes', '120x120');
+    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/apple-touch-icon-120x120.png');
+
+    head.appendChild(icon);
+});
+
+window.addEventListener("load", function() {
+    var head = document.getElementsByTagName('head')[0];
+    var icon = document.createElement('link');
+
+    icon.setAttribute('rel', 'apple-touch-icon');
+    icon.setAttribute('sizes', '180x180');
+    icon.setAttribute('href', 'https://raw.githubusercontent.com/jpcranford/userscripts/main/sites/apple-touch-icon-180x180.png');
+
+    head.appendChild(icon);
+});
