891ad2b627f0dab5b80d5f00a38511653c351616
J.P. Cranford <philipcranford4@gmail.com>
2020-11-16 17:22:17 -0700

    Update Pocket - Apple Touch Icon.user.js

--- Pocket - Apple Touch Icon.user.js
+++ Pocket - Apple Touch Icon.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Apple Touch Icon: Pocket
 // @author       J.P. Cranford
-// @version      1.0
-// @description  Adds an Apple Touch Icon for all pages on Pocket, using their own hosted icon.
+// @version      1.1
+// @description  Adds an Apple Touch Icon for all pages on Pocket, using their mobile app icon.
 // @match        *://*.getpocket.com*
 // @supportURL   https://github.com/jpcranford/userscripts/issues
 // @run-at document-start
@@ -13,8 +13,19 @@ window.addEventListener("load", function() {
     var icon = document.createElement('link');
 
     icon.setAttribute('rel', 'apple-touch-icon');
-    icon.setAttribute('sizes', '144x144');
-    icon.setAttribute('href', 'https://getpocket.com/i/apple-touch-icon/Pocket_AppIcon_@144.png');
+    icon.setAttribute('sizes', '230x230');
+    icon.setAttribute('href', 'https://is3-ssl.mzstatic.com/image/thumb/Purple114/v4/48/04/bc/4804bcb3-e7d2-2272-3b6d-79436277b4eb/AppIcon-0-1x_U007emarketing-0-7-0-0-85-220.png/230x0w.png');
+
+    head.appendChild(icon);
+});
+
+window.addEventListener("load", function() {
+    var head = document.getElementsByTagName('head')[0];
+    var icon = document.createElement('link');
+
+    icon.setAttribute('rel', 'apple-touch-icon');
+    icon.setAttribute('sizes', '460x460');
+    icon.setAttribute('href', 'https://is3-ssl.mzstatic.com/image/thumb/Purple114/v4/48/04/bc/4804bcb3-e7d2-2272-3b6d-79436277b4eb/AppIcon-0-1x_U007emarketing-0-7-0-0-85-220.png/460x0w.png');
 
     head.appendChild(icon);
 });
