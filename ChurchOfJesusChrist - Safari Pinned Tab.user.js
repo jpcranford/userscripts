fa96a20c49f19b297f47c0bea8422a46332a3420
J.P. Cranford <philipcranford4@gmail.com>
2020-11-19 19:52:53 -0700

    Switch to outline vector

--- ChurchOfJesusChrist - Safari Pinned Tab.user.js
+++ ChurchOfJesusChrist - Safari Pinned Tab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Safari Pinned Tab: ChurchOfJesusChrist.org
 // @author       J.P. Cranford
-// @version      1.2.3
+// @version      1.3
 // @description  Adds support for Safari's Pinned Tab and Touch Bar button to the Church's website.
 // @match        *://*.churchofjesuschrist.org/*
 // @supportURL   https://github.com/jpcranford/userscripts/issues
@@ -13,7 +13,7 @@ window.addEventListener("load", function() {
     var icon = document.createElement('link');
 
     icon.setAttribute('rel', 'mask-icon');
-    icon.setAttribute('href', 'https://gist.githubusercontent.com/jpcranford/3248724523eb9290a96175fae33268f6/raw/b90d0877e9c66e6a1a3a3e340205767272d82d2e/churchofjesuschrist-fill.svg');
+    icon.setAttribute('href', 'https://gist.githubusercontent.com/jpcranford/3248724523eb9290a96175fae33268f6/raw/b90d0877e9c66e6a1a3a3e340205767272d82d2e/churchofjesuschrist-outline.svg');
     icon.setAttribute('color', '#006184');
 
     head.appendChild(icon);
